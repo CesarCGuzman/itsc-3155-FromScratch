5666fdbd503faa76cf869813a913c511c5e9c8c9
mjover <mjover@uncc.edu>
2022-11-13 17:55:49 -0500

    Correcting table reference from user to app_user

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS user_scratch_history (
     user_id INT NOT NULL,
     scratch_id INT NOT NULL,
     PRIMARY KEY (user_id, scratch_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES app_user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (scratch_id) REFERENCES scratch(scratch_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
