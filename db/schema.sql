0c4340e810f4a0ee708e87d537799a30eedb591b
Brian Samuels <110922002+BrianSamuels@users.noreply.github.com>
2022-11-13 16:32:43 -0500

    user_scratch_history

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,8 @@
 -- FromScratch SQL Schema here :)
+CREATE TABLE IF NOT EXISTS user_scratch_history (
+    user_id INT NOT NULL,
+    scratch_id INT NOT NULL,
+    PRIMARY KEY (user_id, scratch_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (scratch_id) REFERENCES scratch(scratch_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
