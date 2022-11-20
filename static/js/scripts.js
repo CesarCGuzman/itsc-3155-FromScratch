065c12869c5f40a7137a87b9b2990b67a4571300
CesarCGuzman <cesar.castroguz@gmail.com>
2022-11-20 17:47:41 -0500

    Color buttons now work in canvas

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,11 @@
 const canvas = document.querySelector("canvas"),
+toolBtns = document.querySelectorAll(".tool"),
 colorBtns = document.querySelectorAll(".color-container .btn"),
 ctx = canvas.getContext("2d");
 
 let prevMouseX, prevMouseY,
+selectedTool = "pen",
+selectedColor = "#000",
 isDrawing = false;
 
 window.addEventListener("load", () => {
@@ -15,18 +18,44 @@ const startDrawing = (e) => {
     isDrawing = true;
     prevMouseX = e.offsetX;
     prevMouseY = e.offsetY;
-    ctx.beginPath();
+    ctx.beginPath(); // Creates new drwaing path, so that when you click the line starts where you click and does not draw to you
     ctx.lineWith = 5;
-    ctx.strokeStyle = "#000";
-    ctx.fillStyle = "#000";
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;
 }
 
 const drawing = (e) => {
 
     if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+    if(selectedTool === "pen" || selectedTool === "eraser") {
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
 }
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => {// click event for all the tools
+        //Removes active from currently active - adds it to the btn we clicked
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+
+        //Shows you what tool you clicked on
+        console.log(selectedTool);
+    });
+});
+
+
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        //Removes active from currently active - adds it to the btn we clicked
+        //document.querySelector(".option .selected").classList.remove("selected");
+        //btn.classList.add("selected");
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+        console.log(selectedColor)
+    });
+});
 
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mousemove", drawing);
