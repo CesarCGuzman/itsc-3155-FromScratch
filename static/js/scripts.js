6d0729e68d2c75bd38cfa5b08145069ebd58644f
CesarCGuzman <cesar.castroguz@gmail.com>
2022-11-20 16:16:33 -0500

    Basic working implementation

---
+++ static/js/scripts.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("canvas"),
+ctx = canvas.getContext("2d");
+
+let prevMouseX, prevMouseY,
+isDrawing = false;
+
+window.addEventListener("load", () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+
+const startDrawing = (e) => {
+    isDrawing = true;
+    prevMouseX = e.offsetX;
+    prevMouseY = e.offsetY;
+    ctx.beginPath();
+    ctx.lineWith = 5;
+    ctx.strokeStyle = "#000";
+    ctx.fillStyle = "#000";
+}
+
+const drawing = (e) => {
+    if(!isDrawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => isDrawing = false);
