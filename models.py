96cbc0f2e4bbff59dbad3f6f9bd3155b751788f0
mianjoto <mianjoto@gmail.com>
2022-11-21 14:51:44 -0500

    moving db to top-level for clarity

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class Scratch(db.Model):
+    MAX_CAPTION_LENGTH = 40
+    MAX_DATE_LENGTH = 20
+    __tablename__ = 'scratch'
+    scratch_id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String(MAX_CAPTION_LENGTH), nullable=True)
+    author_id = db.Column(db.Integer, nullable=False)
+    is_comment = db.Column(db.Boolean, nullable=False)
+    date_created = db.Column(db.String(MAX_DATE_LENGTH), nullable=False)
