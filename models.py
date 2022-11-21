87780e03cc9066bde73d8a03e9030153d147f211
Harshica Balu <harshica.balu@gmail.com>
2022-11-21 13:39:51 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'app_user'
+
+    user_id = db.Column(db.Integer,primary_key = True )
+    username = db.Columb(db.String, nullable = False)
+    password = db.Column(db.String, nullable =False)
+    date_created = db.Column(db.String, nullable = False)
+
+    def __init__(self,username,password,date_created) -> None:
+        self.username = username
+        self.password = password
+        self.date_created = date_created
