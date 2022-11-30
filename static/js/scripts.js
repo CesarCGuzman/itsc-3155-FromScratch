7605b6a73e8f3c78de02a27333f9f55884a091ac
CesarCGuzman <cesar.castroguz@gmail.com>
2022-11-29 21:32:21 -0500

    Adding to saving image function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -139,10 +139,15 @@ colorPicker.addEventListener("change", () => {
 });
 
 
-// This currently gives me the image in base64 in the console
+// This currently gives me the image in base64 in the console - I'm adding to it to see if we can send that data to flask
 saveImg.addEventListener("click", () => {
     let image = canvas.toDataURL("image/jpeg", 1.0);
     console.log(image);
+    fetch("/imgProcessing", {
+        "method": "POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify(image),
+    });
 });
 
 canvas.addEventListener("mousedown", startDrawing);
