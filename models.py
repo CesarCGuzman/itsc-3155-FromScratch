c0c59d42cb57ddfcb686d53d84294c7d2256a7ec
mianjoto <mianjoto@gmail.com>
2022-12-08 23:56:27 -0500

    Adding profile picture filename column to database to store profile user picture

--- models.py
+++ models.py
@@ -62,6 +62,7 @@ class AppUser(db.Model):
     MAXIMUM_FRONTEND_PASSWORD_LENGTH: int = 32
     MAXIMUM_DATABASE_PASSWORD_LENGTH: int = 255
     MAXIMUM_BIOGRAPHY_LENGTH: int = 60
+    MAXIMUM_PROFILE_PICTURE_FILENAME_LENGTH: int = 60
 
     user_id = db.Column(
         db.Integer,
@@ -81,6 +82,10 @@ class AppUser(db.Model):
     )
     biography = db.Column(
         db.String(MAXIMUM_BIOGRAPHY_LENGTH),
+        nullable=True
+    ) 
+    profile_picture_filename = db.Column(
+        db.String(MAXIMUM_PROFILE_PICTURE_FILENAME_LENGTH),
         nullable=True
     )
 
